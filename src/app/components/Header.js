00ef3a90f3f9ed18d4f0ae97f324b6a94b6aaeba
NicoletaGheorghe <g.nicole261193@gmail.com>
2025-06-10 12:07:38 +0100

    Resolve conflict and apply stashed changes

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,28 +8,19 @@ export default function Header() {
   const { isLoggedIn, logout } = useAuth();
 
   return (
-    <header className="bg-[var(--color-primary)] text-white px-6 py-2 flex justify-between items-center">
-      <Image
-        src="/images/logo.png"
-        alt="ApartmentsFlow logo"
-        width={150} 
-        height={40}
-      />
-      <nav className="space-x-6 text-lg font-medium">
-        <Link href="/" className="hover:text-[var(--color-accent)]">Home</Link>
-        <Link href="/listings" className="hover:text-[var(--color-accent)]">Listings</Link>
-        <Link href="/favorites" className="hover:text-[var(--color-accent)]">Favorites</Link>
+    <header className="bg-[var(--color-secondary)] text-white px-6 py-1 flex justify-between items-center">
+      <Image src="/images/logo.png" alt="ApartmentsFlow logo" width={200} height={50} priority/>
+      <nav className="space-x-6 text-xl font-semibold">
+        <Link href="/" className="hover:text-[var(--color-primary)] ">Home</Link>
+        <Link href="/apartmentAdd" className="hover:text-[var(--color-primary)] ">Add an apartment</Link>
+        <Link href="/listings" className="hover:text-[var(--color-primary)] ">Listings</Link>
+        <Link href="/favorites" className="hover:text-[var(--color-primary)]">Favorites</Link>
         {!isLoggedIn && (
-          <Link href="/auth/login" className="hover:text-[var(--color-accent)]">Login</Link>
-        )}
+           <Link href="/auth/login" className="hover:text-[var(--color-primary)] ">Login</Link>)}
         {isLoggedIn && (
-          <button
-            onClick={logout}
-            className="hover:text-[var(--color-accent)]"
-          >
-            Logout
-          </button>
-        )}
+          <button onClick={logout}>
+              Logout
+            </button>)}
       </nav>
     </header>
   );
