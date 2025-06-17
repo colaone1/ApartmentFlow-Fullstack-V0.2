59e638b9d0d983168abe32d9237023cd10e28052
colaone1 <samconnor2020@outlook.com>
2025-06-17 17:51:11 +0100

    Remove frontend code and related configurations

--- frontend/src/app/components/Sidebar.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Sidebar() {
-  return (
-    <aside className="w-64 h-screen p-6 bg-white border-r hidden md:block">
-      {/* Sidebar is intentionally left empty for now */}
-    </aside>
-  );
-}
